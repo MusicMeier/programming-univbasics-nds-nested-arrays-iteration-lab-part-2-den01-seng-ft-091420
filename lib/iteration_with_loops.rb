7bbd026274a422fbbcd058e05622aa5b89e0fc41
Music Meier <musicm987@gmail.com>
2020-08-13 23:08:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,15 +3,15 @@ def find_min_in_nested_arrays(src)
   smallest_number_array = []
   row_index = 0 
   while row_index < src.count do
-    element_index = 0 
-    lowest_weather_degrees = ""
-    while element_index < src[row_index].count do
-      if src[row_index][element_index].min < lowest_weather_degrees.min
-        lowest_weather_degrees = src[row_index][element_index]
-      end
-      element_index += 1 
-    end
-    smallest_number_array << lowest_weather_degrees
+    # element_index = 0 
+    # lowest_weather_degrees = ""
+    # while element_index < src[row_index].count do
+    #   if src[row_index][element_index].min < lowest_weather_degrees.min
+    #     lowest_weather_degrees = src[row_index][element_index]
+    #   end
+    #   element_index += 1 
+    # end
+    # smallest_number_array << lowest_weather_degrees
      
    end 
   row_index += 1
